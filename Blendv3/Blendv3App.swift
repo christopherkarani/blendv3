e2ab397a6a646fcbae02b29e2bdbe7c5aeea4647
christopherkarani <chrisbkarani@gmail.com>
2025-05-22 22:57:48 +0300

    Initial Commit

---
+++ Blendv3/Blendv3App.swift
@@ -0,0 +1,17 @@
+//
+//  Blendv3App.swift
+//  Blendv3
+//
+//  Created by Chris Karani on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Blendv3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blendv3Tests/Blendv3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Blendv3Tests.swift
+//  Blendv3Tests
+//
+//  Created by Chris Karani on 22/05/2025.
+//
+
+import Testing
+@testable import Blendv3
+
+struct Blendv3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
